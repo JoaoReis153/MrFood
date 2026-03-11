478ba2944a2c8e5f8e8702fa01f12b8265674dd3
eduhbao-og <eduhbao@duck.com>
2026-03-11 23:59:25 +0000

    basic database setup file updated

--- docs/db_setup.sql
+++ docs/db_setup.sql
@@ -1,8 +1,14 @@
+DROP TABLE IF EXISTS location CASCADE;
+DROP TABLE IF EXISTS restaurant CASCADE;
+DROP TABLE IF EXISTS app_user CASCADE;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS reservation;
+
 CREATE TABLE location(
     location_id serial PRIMARY KEY,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
-    address VARCHAR(100),
+    address VARCHAR(100)
 );
 
 CREATE TABLE restaurant(
@@ -13,13 +19,30 @@ CREATE TABLE restaurant(
     phone VARCHAR(15),
     avg_rating INT,
     sponsored BOOLEAN,
-    location INT references location(location_id)
+    location_id INT references location(location_id)
+);
+
+CREATE TABLE app_user(
+    user_id serial PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE review(
     review_id serial PRIMARY KEY,
     rating INT NOT NULL,
     comment VARCHAR(50),
-    categories VARCHAR()
+    categories VARCHAR(50),
+    timestamp TIMESTAMP,
+    user_id INT references app_user(user_id),
+    restaurant_id INT references restaurant(restaurant_id)
 );
 
+CREATE TABLE reservation(
+    reservation_id serial PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL,
+    people_count INT NOT NULL,
+    user_id INT references app_user(user_id),
+    restaurant_id INT references restaurant(restaurant_id)
+);
