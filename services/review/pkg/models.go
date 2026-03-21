9bb214d14f3d0e764c140bffcdf256a7a4a5ff30
RBray9 <rjbray09@gmail.com>
2026-03-21 17:23:01 +0000

    ADD: Review and ReviewsPage models

--- services/review/pkg/models.go
+++ services/review/pkg/models.go
@@ -1,6 +1,22 @@
 package pkg
 
+import "time"
+
 type Example struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+type Review struct {
+	ReviewID     int       `json:"id"`
+	RestaurantID int       `json:"restaurant_id"`
+	UserID       int       `json:"user_id"`
+	Comment      string    `json:"comment"`
+	Rating       int       `json:"rating"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type ReviewsPage struct {
+	Reviews    []Review   `json:"reviews"`
+	Pagination Pagination `json:"pagination"`
 }
