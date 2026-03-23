6b6d71b23e9eccb3116a579ac32b9a35437943e8
eduhbao-og <164881594+eduhbao-og@users.noreply.github.com>
2026-03-23 10:08:32 +0000

    UPD: models.go for performance

--- services/booking/pkg/models.go
+++ services/booking/pkg/models.go
@@ -6,9 +6,9 @@ type Booking struct {
 	ID           int32     `json:"id"`
 	UserID       int32     `json:"user_id"`
 	RestaurantID int32     `json:"restaurant_id"`
+	PeopleCount  int32     `json:"people_count"`
 	TimeStart    time.Time `json:"time_start"`
 	TimeEnd      time.Time `json:"time_end"`
-	PeopleCount  int32     `json:"people_count"`
 }
 
 type HourSlots struct {
