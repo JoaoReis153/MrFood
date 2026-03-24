723449d68df5fa310245cd8f5b4e2d935f7ef3c9
eduhbao-og <eduhbao@duck.com>
2026-03-24 01:18:11 +0000

    ADD: restaurant_slots to keep track of available slots at a given hour and indexing

--- services/booking/db_setup.sql
+++ services/booking/db_setup.sql
@@ -1,8 +1,23 @@
 CREATE TABLE booking(
     id serial PRIMARY KEY,
     user_id INT NOT NULL,
-    restaurant_id INT NOT NULL
+    restaurant_id INT NOT NULL,
     time_start TIMESTAMP NOT NULL,
     time_end TIMESTAMP NOT NULL,
     people_count INT NOT NULL,
+    CHECK (time_start < time_end),
 );
+
+CREATE TABLE restaurant_slots(
+    id SERIAL PRIMARY KEY,
+    restaurant_id INT NOT NULL,
+    time_start TIMESTAMP NOT NULL,
+    time_end TIMESTAMP NOT NULL,
+    max_slots INT NOT NULL,
+    current_slots INT DEFAULT 0,
+    CHECK (time_start < time_end),
+    CHECK (current_slots <= max_slots),
+    UNIQUE (restaurant_id, week_day),
+);
+
+CREATE INDEX idx_already_booked ON booking (restaurant_id, time_start);
