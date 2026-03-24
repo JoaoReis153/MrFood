ad4ce5aaacb873a83f9c38dfd957ade1525b76d4
eduhbao-og <eduhbao@duck.com>
2026-03-24 23:36:27 +0000

    FIX: some syntax errors and index logic

--- services/booking/db_setup.sql
+++ services/booking/db_setup.sql
@@ -5,7 +5,7 @@ CREATE TABLE booking(
     time_start TIMESTAMP NOT NULL,
     time_end TIMESTAMP NOT NULL,
     people_count INT NOT NULL,
-    CHECK (time_start < time_end),
+    CHECK (time_start < time_end)
 );
 
 CREATE TABLE restaurant_slots(
@@ -16,8 +16,8 @@ CREATE TABLE restaurant_slots(
     max_slots INT NOT NULL,
     current_slots INT DEFAULT 0,
     CHECK (time_start < time_end),
-    CHECK (current_slots <= max_slots),
+    CHECK (current_slots <= max_slots)
 );
 
-CREATE INDEX idx_already_booked ON booking (restaurant_id, time_start);
+CREATE INDEX idx_already_booked ON booking (user_id, restaurant_id, time_start);
 CREATE INDEX idx_restaurant_slots ON restaurant_slots (restaurant_id, time_start);
