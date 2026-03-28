bba76b657401f77633681c07eb518e3f8a177e2a
eduhbao-og <eduhbao@duck.com>
2026-03-28 20:42:42 +0000

    FIX: structure, implemented interfaces and better error handling

--- services/booking/internal/service/service.go
+++ services/booking/internal/service/service.go
@@ -2,129 +2,97 @@ package service
 
 import (
 	"MrFood/services/booking/internal/api/grpc/pb"
-	"MrFood/services/booking/internal/repository"
 	models "MrFood/services/booking/pkg"
 	"context"
+	"errors"
 	"log/slog"
 	"time"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-const MAX_SLOTS int = 15
+const MAX_SLOTS int32 = 15
+
+var (
+	ErrInvalidBooking       = errors.New("invalid booking payload")
+	ErrBookingAlreadyExists = errors.New("booking already exists")
+	ErrForbidden            = errors.New("booking does not belong to user")
+	ErrBookingNotFound      = errors.New("booking not found")
+	ErrFailedWHGet          = errors.New("failed to get working hours")
+)
+
+type BookingRepository interface {
+	CreateBooking(ctx context.Context, booking *models.Booking) (int32, error)
+	DeleteBooking(ctx context.Context, userID, restaurantID int, start time.Time) error
+}
 
 type Service struct {
-	repo   *repository.Repository
-	Client pb.RestaurantServiceClient
+	repo   BookingRepository
+	client pb.RestaurantToBookingServiceClient
 }
 
-func New(repo *repository.Repository, client pb.RestaurantServiceClient) *Service {
-	return &Service{repo: repo, Client: client}
+func New(repo BookingRepository, client pb.RestaurantToBookingServiceClient) *Service {
+	return &Service{repo: repo, client: client}
 }
 
-func (s *Service) CreateBooking(ctx context.Context, booking *models.Booking, working_hours *models.WorkingHours) (*models.Booking, error) {
+func (s *Service) CreateBooking(ctx context.Context, booking *models.Booking) (int32, error) {
+	// check if people count is too high
+	if booking.PeopleCount > MAX_SLOTS {
+		slog.Error("Not enough slots", "people_count", booking.PeopleCount, "max_slots", MAX_SLOTS)
+		return 0, ErrInvalidBooking
+	}
+
 	// truncate start hour to minute 00 or 30
 	booking.TimeStart = booking.TimeStart.UTC().Truncate(30 * time.Minute)
 
-	if booking.TimeStart.Compare(working_hours.TimeStart) < 0 {
-		slog.Error("Booking time is not valid: must be within working hours", "booking_hour_start", booking.TimeStart, "working_hour_start", working_hours.TimeStart)
-		return nil, status.Error(codes.InvalidArgument, "invalid booking start time")
-	}
-
-	// check if booking already exists
-	booking_exists, err := s.repo.CheckBooking(ctx, int(booking.UserID), int(booking.RestaurantID), booking.TimeStart)
+	working_hours, err := s.getWorkingHours(ctx, booking.RestaurantID, booking.TimeStart)
 
 	if err != nil {
-		slog.Error("Internal database error")
-		return nil, status.Error(codes.Internal, err.Error())
+		return 0, err
 	}
 
-	if booking_exists == 1 {
-		slog.Error("Booking already exists")
-		return nil, status.Error(codes.InvalidArgument, "booking already exists")
+	if booking.TimeStart.Before(working_hours.TimeStart) || booking.TimeStart.After(working_hours.TimeEnd) {
+		slog.Error("Invalid booking time", "time_start", booking.TimeStart, "working_time_start", working_hours.TimeStart, "working_time_end", working_hours.TimeEnd)
+		return 0, ErrInvalidBooking
 	}
 
-	// check if there are sufficient available slots
-	exists, max_slots, current_slots, err := s.repo.GetSlots(ctx, int(booking.RestaurantID), booking.TimeStart)
-
-	if err != nil {
-		slog.Error("Internal database error")
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	if exists {
-		if booking.PeopleCount > max_slots-current_slots {
-			slog.Error("Not enough slots", "available_slots", max_slots-current_slots, "people_count", booking.PeopleCount)
-			return nil, status.Error(codes.InvalidArgument, "not enough slots")
-		}
-	} else {
-		if int(booking.PeopleCount) > MAX_SLOTS {
-			slog.Error("Not enough slots", "max_slots", MAX_SLOTS, "people_count", booking.PeopleCount)
-			return nil, status.Error(codes.InvalidArgument, "not enough slots")
-		}
-	}
-
-	// proceed with booking
-	time_end := booking.TimeStart.Add(time.Hour * time.Duration(1))
+	var time_end = booking.TimeStart.Add(time.Hour * time.Duration(1))
 
 	if time_end.After(working_hours.TimeEnd) {
 		time_end = working_hours.TimeEnd
 	}
 
-	booking_id, err := s.repo.CreateBooking(ctx, int(booking.UserID), int(booking.RestaurantID), int(booking.PeopleCount), booking.TimeStart, time_end)
+	booking.TimeEnd = time_end
+
+	booking_id, err := s.repo.CreateBooking(ctx, booking)
 
 	if err != nil {
-		slog.Error("Internal database error")
-		return nil, status.Error(codes.Internal, err.Error())
+		return 0, err
 	}
 
-	if !exists {
-		s.repo.CreateSlots(ctx, int(booking.UserID), int(booking.RestaurantID), MAX_SLOTS, int(booking.PeopleCount), booking.TimeStart, time_end)
-	} else {
-		s.repo.UpdateSlots(ctx, int(booking.RestaurantID), int(current_slots+booking.PeopleCount), booking.TimeStart)
-	}
-
-	booking = &models.Booking{
-		ID:           booking_id,
-		UserID:       booking.UserID,
-		RestaurantID: booking.RestaurantID,
-		PeopleCount:  booking.PeopleCount,
-		TimeStart:    booking.TimeStart,
-		TimeEnd:      time_end,
-	}
-
-	return booking, nil
+	return booking_id, nil
 }
 
 func (s *Service) DeleteBooking(ctx context.Context, booking *models.Booking) error {
 	booking.TimeStart = booking.TimeStart.UTC().Truncate(30 * time.Minute)
 
-	people_count, err := s.repo.DeleteBooking(ctx, int(booking.UserID), int(booking.RestaurantID), booking.TimeStart)
+	err := s.repo.DeleteBooking(ctx, int(booking.UserID), int(booking.RestaurantID), booking.TimeStart)
 
+	return err
+}
+
+func (s *Service) getWorkingHours(ctx context.Context, restaurantID int32, timeStart time.Time) (*models.WorkingHours, error) {
+	res, err := s.client.GetWorkingHours(ctx, &pb.WorkingHoursRequest{
+		RestaurantId: restaurantID,
+		TimeStart:    timestamppb.New(timeStart),
+	})
 	if err != nil {
-		slog.Error("Internal database error")
-		return status.Error(codes.Internal, err.Error())
+		return nil, err
 	}
 
-	_, _, current_slots, err := s.repo.GetSlots(ctx, int(booking.RestaurantID), booking.TimeStart)
-
-	if err != nil {
-		slog.Error("Internal database error")
-		return status.Error(codes.Internal, err.Error())
-	}
-
-	newSlots := current_slots - people_count
-	if newSlots < 0 {
-		newSlots = 0
-	}
-
-	err = s.repo.UpdateSlots(ctx, int(booking.RestaurantID), int(newSlots), booking.TimeStart)
-
-	if err != nil {
-		slog.Error("Internal database error")
-		return status.Error(codes.Internal, err.Error())
-	}
-
-	return nil
+	return &models.WorkingHours{
+		RestaurantID: res.RestaurantId,
+		TimeStart:    res.TimeStart.AsTime(),
+		TimeEnd:      res.TimeEnd.AsTime(),
+	}, nil
 }
