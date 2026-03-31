6155b776aaabc4f1e4bf25473fc85c9da60a8e70
eduhbao-og <eduhbao@duck.com>
2026-03-31 18:26:42 +0100

    ADD: idempotency key and created at

--- services/payment/db_setup.sql
+++ services/payment/db_setup.sql
@@ -1,8 +1,13 @@
 DROP TABLE IF EXISTS receipts;
 
+CREATE TYPE payment_status AS ENUM ('success', 'failed');
+
 CREATE TABLE IF NOT EXISTS receipts (
     id serial PRIMARY KEY,
+    idempotency_key VARCHAR(255) UNIQUE NOT NULL,
     user_id INT NOT NULL,
     ammount DOUBLE PRECISION NOT NULL CHECK (ammount >= 0),
-    payment_description VARCHAR(255)
+    payment_description VARCHAR(255),
+    current_payment_status payment_status NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
 );
--- services/payment/pkg/models.go
+++ services/payment/pkg/models.go
@@ -1,14 +1,21 @@
 package pkg
 
+import "time"
+
 type Receipt struct {
-	ID                 int32   `json:"id"`
-	UserID             int32   `json:"user_id"`
-	Ammount            float32 `json:"ammount"`
-	PaymentDescription string  `json:"payment_description"`
+	ID                 int32     `json:"id"`
+	IdempotencyKey     string    `json:"idempotency_key"`
+	UserID             int32     `json:"user_id"`
+	Ammount            float32   `json:"ammount"`
+	PaymentDescription string    `json:"payment_description"`
+	PaymentStatus      string    `json:"payment_status"`
+	CreatedAt          time.Time `json:"created_at"`
 }
 
 type PaymentRequest struct {
+	IdempotencyKey     string  `json:"idempotency_key"`
 	UserID             int32   `json:"user_id"`
 	Ammount            float32 `json:"ammount"`
 	PaymentDescription string  `json:"payment_description"`
+	PaymentStatus      string  `json:"payment_status"`
 }
