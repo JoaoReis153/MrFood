5713e4cea40788b6195c82359a3f18d08d45b8c6
eduhbao-og <eduhbao@duck.com>
2026-04-16 17:52:11 +0100

    some changes

--- services/payment/db_setup.sql
+++ services/payment/db_setup.sql
@@ -5,9 +5,11 @@ CREATE TYPE payment_status AS ENUM ('success', 'failed');
 CREATE TABLE IF NOT EXISTS receipts (
     id serial PRIMARY KEY,
     idempotency_key VARCHAR(255) UNIQUE NOT NULL,
-    user_id INT NOT NULL,
-    ammount DOUBLE PRECISION NOT NULL CHECK (ammount >= 0),
+    user_id INT NOT NULL UNIQUE,
+    user_email VARCHAR(100) NOT NULL UNIQUE,
+    amount DOUBLE PRECISION NOT NULL CHECK (ammount >= 0),
     payment_description VARCHAR(255),
     current_payment_status payment_status NOT NULL,
+    payment_type VARCHAR(16) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
