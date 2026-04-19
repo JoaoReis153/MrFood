f19aec70b48451a74a693e543d4b08ce3b45ffc6
Reis <80530563+JoaoReis153@users.noreply.github.com>
2026-04-19 23:28:50 +0100

    feat (infra): add terraform vpc module structure

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.0.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+module "vpc" {
+  source       = "./modules/vpc"
+  network_name = "terraform-network"
+}
