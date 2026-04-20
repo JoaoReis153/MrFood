c400dd7269745d1db83f8aa3e114e81e271d03da
Reis <80530563+JoaoReis153@users.noreply.github.com>
2026-04-20 18:43:34 +0100

    chore (infra): added dedicated networks to pods and services

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -2,9 +2,19 @@ resource "google_compute_network" "vpc_network" {
   name = var.network_name
 }
 
-resource "google_compute_subnetwork" "vpc_subnetwork" {
-  name          = "${var.network_name}-subnet"
-  ip_cidr_range = "10.0.0.0/24"
+resource "google_compute_subnetwork" "gke" {
+  name          = "gke-subnet"
   region        = var.region
   network       = google_compute_network.vpc_network.id
+  ip_cidr_range = "10.0.0.0/20"
+
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "10.1.0.0/16"
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "10.2.0.0/20"
+  }
 }
