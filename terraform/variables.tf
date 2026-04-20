c38dcb4de46b3202f509da91cdeb696e55b5742b
Reis <80530563+JoaoReis153@users.noreply.github.com>
2026-04-20 19:49:17 +0100

    chore (infra): defined variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,16 @@ variable "zone" {
 variable "network_name" {
   type = string
 }
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the GKE cluster"
+}
+
+variable "node_machine_type" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
