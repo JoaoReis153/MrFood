84e137a67465de642174dbe7fe924a78b23ecc08
Reis <80530563+JoaoReis153@users.noreply.github.com>
2026-04-20 18:39:24 +0100

    chore (infra): add /24 subnetwork

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,3 +1,10 @@
 resource "google_compute_network" "vpc_network" {
   name = var.network_name
 }
+
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+  name          = "${var.network_name}-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,4 +1,7 @@
 variable "network_name" {
-  description = "Name of the VPC network"
-  type        = string
+  type = string
+}
+
+variable "region" {
+  type = string
 }
