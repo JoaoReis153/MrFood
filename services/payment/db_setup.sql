ed74caf92836f94cdcbf814b9830f40dea6d0201
Reis <joaosimoesreis.jsr@gmail.com>
2026-05-11 11:29:25 +0100

    fix: make db_setup.sql idempotent

--- services/payment/db_setup.sql
+++ services/payment/db_setup.sql
@@ -1,16 +1,17 @@
-DROP TABLE IF EXISTS receipts;
-
-CREATE TYPE payment_status AS ENUM ('success', 'failed');
+DO $$ BEGIN
+    CREATE TYPE payment_status AS ENUM ('success', 'failed');
+EXCEPTION WHEN duplicate_object THEN NULL;
+END $$;
 
 CREATE TABLE IF NOT EXISTS receipts (
-    id serial PRIMARY KEY,
-    idempotency_key VARCHAR(255) UNIQUE NOT NULL,
-    request_hash TEXT NOT NULL,
-    user_id BIGINT NOT NULL,
-    user_email VARCHAR(100) NOT NULL,
-    amount NUMERIC(12,2) NOT NULL CHECK (amount >= 0),
-    payment_description VARCHAR(255),
+    id                     SERIAL         PRIMARY KEY,
+    idempotency_key        VARCHAR(255)   UNIQUE NOT NULL,
+    request_hash           TEXT           NOT NULL,
+    user_id                BIGINT         NOT NULL,
+    user_email             VARCHAR(100)   NOT NULL,
+    amount                 NUMERIC(12,2)  NOT NULL CHECK (amount >= 0),
+    payment_description    VARCHAR(255),
     current_payment_status payment_status NOT NULL,
-    payment_type VARCHAR(16) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    payment_type           VARCHAR(16)    NOT NULL,
+    created_at             TIMESTAMP      DEFAULT NOW()
 );
